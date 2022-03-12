f2bd3917fab38d649e2f71e965d746c5b05889f3
Aaryan562 <82304628+Aaryan562@users.noreply.github.com>
2022-03-12 22:00:18 +0530

    Add files via upload

--- cv.py
+++ cv.py
@@ -119,3 +119,51 @@ blue_channel = cv2.LUT(blue_channel, increaseLookupTable).astype(np.uint8)
 win= cv2.merge((blue_channel, green_channel, red_channel))
 
 #Winter Filter
+
+# Rotate the image
+from scipy import ndimage
+angle=45
+rotated = ndimage.rotate(img3, angle)
+cv2_imshow(rotated)
+
+________________________________________________________________
+
+import numpy as np
+import cv2
+
+	
+#reading the image
+input_image = cv2.imread('/content/photo-1535463731090-e34f4b5098c5.jpg')
+
+#resizing the image according to our need
+# resize() function takes 2 parameters,
+# the image and the dimensions
+input_image = cv2.resize(input_image, (480, 480))
+
+# Extracting the height and width of an image
+rows, cols = input_image.shape[:2]
+
+# generating vignette mask using Gaussian
+# resultant_kernels
+X_resultant_kernel = cv2.getGaussianKernel(cols,200)
+Y_resultant_kernel = cv2.getGaussianKernel(rows,200)
+
+#generating resultant_kernel matrix
+resultant_kernel = Y_resultant_kernel * X_resultant_kernel.T
+
+#creating mask and normalising by using np.linalg
+# function
+mask = 255 * resultant_kernel / np.linalg.norm(resultant_kernel)
+output = np.copy(input_image)
+
+# applying the mask to each channel in the input image
+for i in range(3):
+	output[:,:,i] = output[:,:,i] * mask
+	
+#displaying the original image
+cv2_imshow(input_image)
+
+#displaying the vignette filter image
+cv2_imshow(output)
+
+
